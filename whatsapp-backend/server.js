9a999759bf6a29feb6f1f93ed5847da4371d0e35
unknown <fvkfuad@gmail.com>
2021-09-15 21:27:44 +0530

    initialize backend api

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,17 @@
+// importing
+import express from 'express'
+
+// app config
+const app = express();
+const port = process.env.PORT || 9000
+
+//middleware
+
+// DB config
+
+
+// api routes
+app.get('/', (req, res)=>res.status(200).send('Helo World. my name is fuad'));
+
+// listen
+app.listen(port, ()=>console.log(`Listen on localhost: ${port}`));
